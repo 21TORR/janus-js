71d3cb1ec7591c74c59d90c4131f20d3e41f0f69
Jannik Zschiesche <j.zschiesche@21torr.com>
2024-07-11 16:11:31 +0200

    Add init for tsconfig.json in eslint

--- _init/next-js/eslint.config.mjs
+++ _init/next-js/eslint.config.mjs
@@ -1,9 +1,21 @@
 import eslint21TORR from "@21torr/janus/eslit/base";
 import * as storybookPlugin from "eslint-plugin-storybook";
 import * as nextChecks from "next/core-web-vitals";
+import {fileURLToPath} from "node:url";
+import {join, dirname} from "node:path";
 
 export default [
 	nextChecks,
 	storybookPlugin,
 	eslint21TORR,
+	{
+		languageOptions: {
+			parserOptions: {
+				project: join(
+					dirname(fileURLToPath(import.meta.url)),
+					"tsconfig.json",
+				),
+			},
+		},
+	}
 ];
