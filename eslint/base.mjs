84d7a1e8834320297a4310927281f8701b77a403
Jannik Zschiesche <j.zschiesche@21torr.com>
2024-07-11 12:42:07 +0200

    Finalize base ESLint config

--- eslint/base.mjs
+++ eslint/base.mjs
@@ -12,6 +12,7 @@ export default [
 			},
 		},
 		rules: {
+			// region ESLint: Possible Problems
 			"no-await-in-loop": "error",
 			"no-constructor-return": "error",
 			"no-duplicate-imports": "warn",
@@ -19,6 +20,29 @@ export default [
 			"no-template-curly-in-string": "warn",
 			"no-use-before-define": "warn",
 			"no-useless-assignment": "warn",
+			// endregion
+
+			// region ESLint: Suggestions
+			"block-scoped-var": "error",
+			"default-case-last": "warn",
+			"default-param-last": "warn",
+			"eqeqeq": ["error", "always", {
+				"null": "ignore",
+			}],
+			"max-depth": ["warn", 4],
+			"max-params": ["warn", 5],
+			"no-alert": "error",
+			"no-array-constructor": "error",
+			"no-bitwise": "warn",
+			"no-caller": "warn",
+			"no-console": "warn",
+			"no-eval": "error",
+			"no-extend-native": "error",
+			// endregion
+
+			// region ESLint: Layout & Formatting
+			"unicode-bom": ["error", "never"],
+			// endregion
 
 			// region JSDoc
 			"jsdoc/check-param-names": ["error", {
