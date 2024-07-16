a5737e8acd0be3de9bf46214cc8c4aaeacd90ca8
Jannik Zschiesche <j.zschiesche@21torr.com>
2024-07-16 15:44:05 +0200

    Fix loading of storybook plugin

--- eslint/next-js.mjs
+++ eslint/next-js.mjs
@@ -1,8 +1,12 @@
 import base from './base.mjs';
 import nextPlugin from "@next/eslint-plugin-next";
 import { FlatCompat } from '@eslint/eslintrc'
+import * as storybookPlugin from "eslint-plugin-storybook";
 
-const compat = new FlatCompat();
+const compat = new FlatCompat({
+	baseDirectory: import.meta.dirname,
+});
+
 
 export default [
 	...compat.config({
@@ -12,6 +16,12 @@ export default [
 		// .eslintignore is not supported with flat config, make sure to ignore also other build and test folders
 		ignorePatterns: ['!.storybook', 'storybook-static'],
 	}),
+	{
+		name: "Storybook",
+		plugins: {
+			"eslint-plugin-storybook": storybookPlugin,
+		},
+	},
 	{
 		name: "Next.js",
 		plugins: {
