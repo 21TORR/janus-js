47f2184c7d93441cbb85903f5030fe900f02bd50
Jannik Zschiesche <j.zschiesche@21torr.com>
2024-11-06 10:31:48 +0100

    Use flat config structure for ESLint Storybook config

--- eslint/next-js.mjs
+++ eslint/next-js.mjs
@@ -1,27 +1,10 @@
 import base from './base.mjs';
 import nextPlugin from "@next/eslint-plugin-next";
-import { FlatCompat } from '@eslint/eslintrc'
-import * as storybookPlugin from "eslint-plugin-storybook";
-
-const compat = new FlatCompat({
-	baseDirectory: import.meta.dirname,
-});
+import storybook from "eslint-plugin-storybook";
 
 
 export default [
-	...compat.config({
-		extends: [
-			'plugin:storybook/recommended',
-		],
-		// .eslintignore is not supported with flat config, make sure to ignore also other build and test folders
-		ignorePatterns: ['!.storybook', 'storybook-static'],
-	}),
-	{
-		name: "Storybook",
-		plugins: {
-			"eslint-plugin-storybook": storybookPlugin,
-		},
-	},
+	...storybook.configs["flat/recommended"],
 	{
 		name: "Next.js",
 		plugins: {
